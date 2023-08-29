28c2ea5a8266d2702e46d6af08ed01c2be67799e
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-08-29 11:11:45 -0300

    a

--- includes/topo.php
+++ includes/topo.php
@@ -9,8 +9,8 @@
     <?php if (isset($_SESSION['login'])): ?> 
     <div class="card-body text-right">
         Olá <?php echo $_SESSION['login']['usuario']['nome']?>! 
-        <a href="core/usuario_repositorio.php?acao-logout" 
+        <a href="core/usuario_repositorio.php?acao=logout" 
         class="btn btn-link btn-sm" role="button">Sair</a>
     </div>
-    <?php endif ?>
+    <?php endif; ?>
 </div>
