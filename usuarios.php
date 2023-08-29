06da690605089e8da06ec3c2301b2c8eb6746737
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-08-29 11:33:46 -0300

    at-php

--- usuarios.php
+++ usuarios.php
@@ -58,3 +58,47 @@
                             );
                             
                     ?>
+                    <table class="table table-bordered table-hover table-striped
+                                    table-responsive {-sm-md|-lg-x1}">
+                    <thead>
+                        <tr>
+                            <td>Nome</td>
+                            <td>E-mail</td>
+                            <td>Data cadastro</td>
+                            <td>Ativo</td>
+                            <td>Administrador</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            foreach ($result as $entidade):
+                                $data = date_create ($entidade ['data_criacao']); 
+                                $data = date_format($data, 'd/m/Y H:i:s');
+                        ?>
+                        <tr>
+                            <td><?php echo $entidade ['nome'] ?></td>
+                            <td><?php echo $entidade ['email'] ?></td>
+                            <td><?php echo $data?></td>
+                            <td><a href='core/usuario_repositorio.php?acao-status&id=<?php echo $entidade ['id']?> 
+                            &valor=<?php echo $entidade ['ativo']?>'><?php echo ($entidade ['ativo']==1) ? 
+                            'Desativar' : 'Ativar'; ?> </a></td>
+                            <td><a href='core/usuario_repositorio.php?acao-adm&id=<?php echo $entidade ['id']?> 
+                            &valor=<?php echo $entidade ['adm']?>'><?php echo ($entidade ['adm']==1) ? 'Rebaixar' : 
+                            'Promover'; ?> </a></td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <?php
+                        include 'includes/rodape.php';
+                    ?>
+                </div>
+            </div>
+        </div>
+        <script src="lib/bootstrap-4.2.1-dist/js/bootstrap.min.js"></script>        
+    </body>
+</html>
