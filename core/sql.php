b7dd7b99f273a4390f58ab05776ea8f13e12e68c
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-09-12 12:00:09 -0300

    at

--- core/sql.php
+++ core/sql.php
@@ -33,7 +33,7 @@ function update (string $entidade, array $dados, array $criterio = []): string{
 
 function delete (string $entidade, array $criterio = []) : string{
 
-    $instrucao = "DELETE /*FROM*/ {$entidade}";
+    $instrucao = "DELETE FROM {$entidade}";
 
     if (!empty($criterio)){
 
