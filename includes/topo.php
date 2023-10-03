1f3089b96571b73ab0ba4dc4fa84d48cbc438275
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-10-03 10:38:07 -0300

    at-php

--- includes/topo.php
+++ includes/topo.php
@@ -7,7 +7,7 @@
     </div>
     <?php if (isset($_SESSION['login'])): ?> 
         <div class="card-body text-right">
-        Olá <?php echo $_SESSION['login']['usuario']['nome']?>! 
+        Olá, <?php echo $_SESSION['login']['usuario']['nome']?>! 
         
         <a href="core/usuario_repositorio.php?acao=logout" class="btn btn-link btn-sm" role="button">Sair</a>
         </div>
